d3719036d02e53964edac1ac23cd319a8ec46dcb
Abhyuday-06 <bugattiveyronagera@gmail.com>
2025-01-21 01:14:25 +0530

    Updated code for quiz.html

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 import os
+from dotenv import load_dotenv
 import secrets
 
-secret = secrets.token_hex(32)
+load_dotenv()
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", secret)
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///data.db")
+    SECRET_KEY = os.getenv("SECRET_KEY", secrets.token_hex(32))  # Use .env value or generate a fallback key
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///data.db",  # Fallback to SQLite for local development if DATABASE_URL isn't set
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
+    DEBUG = False
+    ENV = 'production'
